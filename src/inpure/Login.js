3aca09d4abf84b6ef5169c8f79b05fd3d6ed8e18
kofi <fred.nedjoh@coxautoinc.com>
2019-07-15 13:50:55 -0400

    ui tweaks

--- src/inpure/Login.js
+++ src/inpure/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import PropTypes from 'prop-types';
 import get from 'lodash.get';
 
 import Card from '../pure/Card';
